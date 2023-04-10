edbbcb6b67117dfe12595e7b5401842d50b5bd95
Fyodor <fyezhlo@gmail.com>
2023-04-10 15:11:18 +0300

    robot fixed

--- src/main/java/org/example/compilers/robot/controllers/NoTerminatingCharacterException.java
+++
@@ -1,7 +0,0 @@
-package org.example.compilers.robot.controllers;
-
-public class NoTerminatingCharacterException extends Exception{
-    public NoTerminatingCharacterException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/example/compilers/robot/controllers/NotEmptyAfterTermCharException.java
+++
@@ -1,7 +0,0 @@
-package org.example.compilers.robot.controllers;
-
-public class NotEmptyAfterTermCharException extends Exception{
-    public NotEmptyAfterTermCharException(String message) {
-        super(message);
-    }
-}
